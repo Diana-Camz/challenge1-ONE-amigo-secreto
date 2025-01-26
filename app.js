811221b189aa75f27f76d5c61cf3da311bcc177d
Diana Campos <dcampos0495@gmail.com>
2025-01-26 12:45:29 -0600

    crea las funciones que permitan encontrar al amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log('jalando')
+let amigos = [];
+let lista = document.getElementById('listaAmigos');
+let resultado = document.getElementById('resultado');
+
+
+const agregarAmigo = () => {
+    let amigo = document.getElementById('amigo').value;
+   if(amigo == "") {
+    alert("Por favor, inserta un nombre!!");
+   } else {
+    amigos.push(amigo);
+    lista.innerHTML = "";
+    mostrarLista();
+    document.getElementById('amigo').value = "";
+ }
+}
+
+const mostrarLista = () => {
+    for(let i = 0; i < amigos.length; i++){
+        let nuevoItem = document.createElement('li');
+        nuevoItem.innerHTML = amigos[i];
+        lista.appendChild(nuevoItem);
+    }
+}
+
+
+const sortearAmigo = () => {
+    let amigoSecreto = document.createElement('li');
+    if(amigos.length == 0){
+        alert('Aun no has agregado ningun Amigo!!');
+    } else {
+        let numRandom = Math.floor(Math.random() * amigos.length) 
+            amigoSecreto.innerHTML = ` El amigo secreto es ${amigos[numRandom]}!!`;
+            resultado.appendChild(amigoSecreto);
+
+    }
+
+}
